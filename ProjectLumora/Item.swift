9a70a718d0c72a75c44818e985e22239991c47ef
Aman <aman.nair9302@gmail.com>
2026-04-11 21:16:28 +0530

    Initial Commit

---
+++ ProjectLumora/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ProjectLumora
+//
+//  Created by Aman Nair on 11/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
